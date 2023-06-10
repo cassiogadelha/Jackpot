c60b3a2fabf31e8678a729b151a5781ad7c33aae
Cássio Gadêlha <cassiogadelha15@gmail.com>
2023-06-10 19:39:26 -0300

    PullHandle

--- Jackpot/Assets/Scripts/GameManager.cs
+++ Jackpot/Assets/Scripts/GameManager.cs
@@ -37,8 +37,108 @@ public class GameManager : MonoBehaviour
 
         if (rows[0].rowStopped && rows[1].rowStopped && rows[2].rowStopped)
         {
+
             prizeText.enabled = true;
             prizeText.text = $"Prize: {prizeValue}";
         }
+    }
+
+    private void OnMouseDown()
+    {
+        if(rows[0].rowStopped && rows[1].rowStopped && rows[2].rowStopped)
+        {
+
+        }
+    }
+
+    private IEnumerator PullHandle()
+    {
+        for(int i = 0; i < 15; i+=5)
+        {
+            handle.Rotate(0f, 0f, i);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        HandlePulled();
+
+        for (int i = 0; i < 15; i += 5)
+        {
+            handle.Rotate(0f, 0f, -i);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void CheckResults()
+    {
+        if (rows[0].stoppedSlot == "Diamond" && rows[1].stoppedSlot == "Diamond" && rows[2].stoppedSlot == "Diamond")
+        {
+            prizeValue = 200;
+        }else if(rows[0].stoppedSlot == "Crow" && rows[1].stoppedSlot == "Crow" && rows[2].stoppedSlot == "Crow")
+        {
+            prizeValue = 400;
+        }
+        else if (rows[0].stoppedSlot == "Melon" && rows[1].stoppedSlot == "Melon" && rows[2].stoppedSlot == "Melon")
+        {
+            prizeValue = 600;
+        }
+        else if (rows[0].stoppedSlot == "Bar" && rows[1].stoppedSlot == "Bar" && rows[2].stoppedSlot == "Bar")
+        {
+            prizeValue = 800;
+        }
+        else if (rows[0].stoppedSlot == "Seven" && rows[1].stoppedSlot == "Seven" && rows[2].stoppedSlot == "Seven")
+        {
+            prizeValue = 1500;
+        }
+        else if (rows[0].stoppedSlot == "Cherry" && rows[1].stoppedSlot == "Cherry" && rows[2].stoppedSlot == "Cherry")
+        {
+            prizeValue = 3000;
+        }
+        else if (rows[0].stoppedSlot == "Dfilitto" && rows[1].stoppedSlot == "Dfilitto" && rows[2].stoppedSlot == "Dfilitto")
+        {
+            prizeValue = 400;
+        }else if (rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Diamond" ||
+                  rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Diamond" ||
+                  rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Diamond")
+        {
+            prizeValue = 100;
+        }
+        else if (rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Crow" ||
+                  rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Crow" ||
+                  rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Crow")
+        {
+            prizeValue = 100;
+        }
+        else if (rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Melon" ||
+                  rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Melon" ||
+                  rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Melon")
+        {
+            prizeValue = 500;
+        }
+        else if (rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Bar" ||
+                  rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Bar" ||
+                  rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Bar")
+        {
+            prizeValue = 700;
+        }
+        else if (rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Seven" ||
+                  rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Seven" ||
+                  rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Seven")
+        {
+            prizeValue = 1000;
+        }
+        else if (rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Cherry" ||
+                  rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Cherry" ||
+                  rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Cherry")
+        {
+            prizeValue = 2000;
+        }
+        else if (rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Dfilitto" ||
+                  rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Dfilitto" ||
+                  rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Dfilitto")
+        {
+            prizeValue = 4000;
+        }
+
+        resultsChecked = true;
     }
 }
